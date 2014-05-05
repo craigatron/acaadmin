2bc5a70847559808cad36aeceb051dda25822111
Craig Martek <craigmartek@gmail.com>
2014-05-04 23:37:23 -0400

    fix author

--- songs/views.py
+++ songs/views.py
@@ -32,8 +32,20 @@ def delete(request, song_id):
 @login_required
 def index(request):
   songs = models.Song.objects.all()
+  all_votes = models.Vote.objects.all()
   states = models.Song.STATES
-  dictionary = {k: songs.filter(state=states.index(k)) for k in states}
+  dictionary = {k: songs.filter(state=states.index(k))
+                for k in states if 'proposed' != k}
+  proposed = {s.pk : {'song': s} for s in songs.filter(state=states.index('proposed'))}
+  for _, v in proposed.items():
+    votes = all_votes.filter(song=v['song'])
+    vote_counts = [0, 0, 0]
+    for vote in votes:
+      vote_counts[vote.vote] += 1
+    v['votes'] = vote_counts
+    user_vote = all_votes.filter(song=v['song'], user=request.user)[:1]
+    v['user_vote'] = user_vote.get().vote if user_vote else -1
+  dictionary['proposed'] = proposed.values()
 
   return render(request, 'list.html', dictionary,
       context_instance=RequestContext(request))

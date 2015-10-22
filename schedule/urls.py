8a813a44415674f226d783faa7a301933eb5e1a8
Craig Martek <craigmartek@gmail.com>
2015-10-21 18:40:14 -0700

    ids can be more than one digit, duh

--- schedule/urls.py
+++ schedule/urls.py
@@ -4,6 +4,6 @@ from schedule import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='schedule_index'),
     url(r'^feed/?$', views.event_feed),
-    url(r'^(?P<event_id>[^/]?)/?$', views.event_view),
-    url(r'^(?P<event_id>[^/]?)/change/?$', views.change_attendance),
+    url(r'^(?P<event_id>[0-9]+)/?$', views.event_view),
+    url(r'^(?P<event_id>[0-9]+)/change/?$', views.change_attendance),
 )
